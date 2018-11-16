702a14deaa80558f3f45f37e83702a462375920d
Julian Garcia <julian.pareja@hotmail.com>
2018-11-16 16:34:10 +0000

    updated categories controller

--- LIG_IndustryCategoryMap/aura/LIG_IndustryCategoryMapping/LIG_IndustryCategoryMappingController.js
+++ LIG_IndustryCategoryMap/aura/LIG_IndustryCategoryMapping/LIG_IndustryCategoryMappingController.js
@@ -1,29 +1,37 @@
 ({
     doInit: function(component,event,helper) {
-        //validate and get the data we need from the server
-        helper.validateUser(component, event);
-        helper.getParentAndSubCategories(component, event);
-        helper.getMinorCategories(component, event);       
+        
+         helper.validateUser(component, event);//check if the user is a VDT member
+         helper.getParentAndSubCategories(component, event);//call server to get picklist values for parent and sub
+         helper.getMinorCategories(component, event); //call server to get picklist values for minor      
     },
     
     onControllerFieldChange: function(component, event, helper) {
         // get selected controller field value
         var controllerValueKey = event.getSource().get("v.value"); 
         var depnedentFieldMap = component.get("v.depnedentFieldMap");
-        
+        component.set("v.displayIndustry",false);//hide the default value of the industry picklist
         if (controllerValueKey != '--- None ---') {
             var ListOfDependentFields = depnedentFieldMap[controllerValueKey];
             
             if(ListOfDependentFields.length > 0){
-                component.set("v.bDisabledDependentFld", false);  
-                helper.fetchDepValues(component, ListOfDependentFields,"v.subPicklistVal");    
+                component.set("v.bDisabledDependentFld", false);//enable edit on sub category
+                component.set("v.fields.Industry_Sub_Category__c","---None---");//reset picklist
+                component.set("v.fields.Industry_Minor_Category__c","---None---");//reset picklist
+                helper.fetchDepValues(component, ListOfDependentFields,"v.subPicklistVal");//get the dependent field values
+                //check if the save button is enabled and disable to prevent users from saving ---none--- values
+                if(!component.get("v.disableSave")){
+                   component.set("v.disableSave",true);
+                }
             }else{
+                //if there are no dependent fields show the below
                 component.set("v.bDisabledDependentFld", true); 
                 component.set("v.subPicklistVal", ['--- None ---']);
                 component.set("v.minorPicklistVal", ['--- None ---']);
             }  
             
         } else {
+            //if there are no dependent fields show the below
             component.set("v.bDisabledDependentFld", true);
             component.set("v.subPicklistVal", ['--- None ---']);
             component.set("v.minorPicklistVal", ['--- None ---']);          
@@ -33,36 +41,54 @@
     onSubControllerFieldChange: function(component, event, helper) {     
         var controllerValueKey = event.getSource().get("v.value"); // get selected controller field value
         var subdepnedentFieldMap = component.get("v.subDepnedentFieldMap");
+        component.set("v.displaySubCateg",false);
         if (controllerValueKey != '--- None ---') {
+            //get the picklist values based on the key
             var ListOfDependentFields = subdepnedentFieldMap[controllerValueKey];
-            
+            if(!component.get("v.disableSave")){
+                component.set("v.disableSave",true);
+                component.set("v.fields.Industry_Minor_Category__c","--- None ---");
+                component.set("v.minorPicklistVal", ['--- None ---']);
+            }
             if(ListOfDependentFields.length > 0){
-                component.set("v.bDisabledSubDependentFld", false);  
-                helper.fetchDepValues(component, ListOfDependentFields,"v.minorPicklistVal");    
+                component.set("v.bDisabledSubDependentFld", false); //enable edit on minor category
+                //get the values for the minor category picklist
+                 
+                helper.fetchDepValues(component, ListOfDependentFields,"v.minorPicklistVal"); 
             }else{
+                //if there are not values disbable edit and reset picklist
                 component.set("v.bDisabledSubDependentFld", true); 
                 component.set("v.minorPicklistVal", ['--- None ---']);
             }  
             
         } else {
+            //if there are not values disbable edit and reset picklist
             component.set("v.bDisabledDependentFld", true);
             component.set("v.minorPicklistVal", ['--- None ---']);            
         }
     },
     
     onSubDependentFieldChange: function(component, event, helper){
+        //if the minor category value changes then we can enable save button
+        component.set("v.displayMinorCat",false);
         component.set("v.disableSave",false);
     },
     
     edit: function(component,event,helper) {
-        component.set("v.bDisabledParentDependentFld", false);
-        component.set("v.showEdit", false);
-        component.set("v.showCancel", true);
+        component.set("v.bDisabledParentDependentFld", false);//enable edit on industry picklist
+        component.set("v.bDisabledDependentFld", false);//enable edit on sub category
+        component.set("v.showEdit", false);//hide the edit button
+        component.set("v.showCancel", true);//show the cancel button
         
-        if(component.get("v.disableSave")){           
-            component.set("v.fields.Industry","00 - Please Select");
-            component.set("v.fields.Industry_Sub_Category__c","---None---");
-            component.set("v.fields.Industry_Minor_Category__c","---None---");
+        
+        if(component.get("v.disableSave")){
+            //first time they click on edit we want to enable edit on industry and show the current value of the record if there is one
+            if(component.get("v.fields.Industry")!=null){
+                var controllerValueKey = component.get("v.fields.Industry"); 
+                var depnedentFieldMap = component.get("v.depnedentFieldMap");
+                var ListOfDependentFields = depnedentFieldMap[controllerValueKey];
+                helper.fetchDepValues(component, ListOfDependentFields,"v.subPicklistVal");
+            }
         }
    	},
     
